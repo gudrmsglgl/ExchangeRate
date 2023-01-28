f25102d96b982ee647ed306c5155c1af3a462763
gudrmsglgl <gudrmsglgl@gmail.com>
2023-01-29 03:04:24 +0900

    feat #comment UI - Home - 다크모드 지원 - UI 개발 (Model 샘플로 개발) - TextField 포커싱 처리 - 환율 소수점 처리 완료 - 환율 선택 변경에 따른 상태 변경 완료 - TimeStamp 처리 완료 - Preview 지원

--- app/src/main/java/com/wirebarley/exchange/ui/theme/Theme.kt
+++ app/src/main/java/com/wirebarley/exchange/ui/theme/Theme.kt
@@ -5,17 +5,19 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 
 private val DarkColorPalette = darkColors(
     primary = Purple200,
     primaryVariant = Purple700,
-    secondary = Teal200
+    secondary = Purple150
 )
 
 private val LightColorPalette = lightColors(
     primary = Purple500,
     primaryVariant = Purple700,
-    secondary = Teal200
+    secondary = Black
 
     /* Other default colors to override
     background = Color.White,
@@ -29,6 +31,17 @@ private val LightColorPalette = lightColors(
 
 @Composable
 fun ExchangeRateTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
+    val systemUiController = rememberSystemUiController()
+    if (darkTheme) {
+        systemUiController.setSystemBarsColor(
+            color = Color.Black
+        )
+    } else {
+        systemUiController.setSystemBarsColor(
+            color = Color.White
+        )
+    }
+
     val colors = if (darkTheme) {
         DarkColorPalette
     } else {
