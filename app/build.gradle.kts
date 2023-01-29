57a2690a67c74f0ea65af435d1044a1eecce53b3
gudrmsglgl <gudrmsglgl@gmail.com>
2023-01-29 15:20:45 +0900

    setup #comment buildSrc kts 셋팅

---
+++ app/build.gradle.kts
@@ -0,0 +1,67 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.wirebarley.exchange"
+    compileSdk = Configs.compileSdkVersion
+
+    defaultConfig {
+        applicationId = "com.wirebarley.exchange"
+        minSdk = Configs.minSdkVersion
+        targetSdk = Configs.targetSdkVersion
+        versionCode = Configs.versionCode
+        versionName = Configs.versionName
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        vectorDrawables {
+            useSupportLibrary = true
+        }
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.1.1"
+    }
+    packagingOptions {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
+}
+
+dependencies {
+
+    implementation(Dependencies.KTX.core)
+
+    implementation(Dependencies.LifeCycle.runtimeKtx)
+
+    implementation(Dependencies.Compose.activity)
+    implementation(Dependencies.Compose.ui)
+    implementation(Dependencies.Compose.preview)
+    implementation(Dependencies.Compose.material)
+
+    testImplementation(TestDependencies.junit)
+    androidTestImplementation(TestDependencies.junitAndroidx)
+    androidTestImplementation(TestDependencies.espressoCore)
+    androidTestImplementation(Dependencies.Compose.uiTestJunit4)
+
+    debugImplementation(Dependencies.Compose.uiTooling)
+    debugImplementation(Dependencies.Compose.uiTestManifest)
+}
---
+++ buildSrc/src/main/kotlin/TestDependencies.kt
@@ -0,0 +1,5 @@
+object TestDependencies {
+    val junit by lazy { "junit:junit:4.13.2" }
+    val junitAndroidx by lazy { "androidx.test.ext:junit:1.1.5" }
+    val espressoCore by lazy { "androidx.test.espresso:espresso-core:3.5.1" }
+}
